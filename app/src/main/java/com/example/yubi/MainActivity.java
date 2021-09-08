d84abc78735de6022fd9fa2b5d05950d29091503
NoYoN1 <uilse1122@gmail.com>
2021-09-08 23:27:06 +0900

    intent

--- app/src/main/java/com/example/yubi/MainActivity.java
+++ app/src/main/java/com/example/yubi/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.yubi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView click = findViewById(R.id.textbutton);
+        
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Game.class);
+                startActivity(intent);
+            }
+        });
     }
 }
