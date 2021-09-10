7ef181af502cb944616064842963022d60a185ca
NoYoN1 <uilse1122@gmail.com>
2021-09-10 15:32:27 +0900

    array list problem solve

--- app/src/main/java/com/example/yubi/Game.java
+++ app/src/main/java/com/example/yubi/Game.java
@@ -12,9 +12,6 @@ import java.util.ArrayList;
 
 public class Game extends AppCompatActivity {
 
-    boolean p1f1 = false, p1f2 = false, p1f3 = false, p2f1 = false, p2f2 = false, p2f3 = false;
-    ArrayList<Integer> p1ArrayList = new ArrayList<>();
-    ArrayList<Integer> p2ArrayList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +20,9 @@ public class Game extends AppCompatActivity {
         TextView p1finger1 = findViewById(R.id.p1finger1);
         TextView p1finger2 = findViewById(R.id.p1finger2);
         TextView p1finger3 = findViewById(R.id.p1finger3);
-        TextView p2finger1 = findViewById(R.id.p1finger1);
-        TextView p2finger2 = findViewById(R.id.p1finger2);
-        TextView p2finger3 = findViewById(R.id.p1finger3);
+        TextView p2finger1 = findViewById(R.id.p2finger1);
+        TextView p2finger2 = findViewById(R.id.p2finger2);
+        TextView p2finger3 = findViewById(R.id.p2finger3);
         TextView p1point = findViewById(R.id.p1point);
         TextView p2point = findViewById(R.id.p2point);
         TextView round = findViewById(R.id.round);
@@ -33,123 +30,46 @@ public class Game extends AppCompatActivity {
         TextView p2order = findViewById(R.id.p2order);
         Button cbtn = findViewById(R.id.checkButton);
         Button rbtn = findViewById(R.id.reset);
-
-
-//        p1click();
-
-            p1finger1.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    p1f1 = true;
-                    p1ArrayList.add(1);
-                }
+        ArrayList<Integer> p1ArrayList = new ArrayList<>();
+        ArrayList<Integer> p2ArrayList = new ArrayList<>();
+        int nextRound = 0;
+        //////////////// player 1 = blue ////////////////
+            p1finger1.setOnClickListener(view -> {
+                p1ArrayList.add(1);
             });
-            p1finger2.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    p1f2 = true;
-                    p1ArrayList.add(2);
-                }
+            p1finger2.setOnClickListener(view -> {
+                p1ArrayList.add(2);
             });
-            p1finger3.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    p1f3 = true;
-                    p1ArrayList.add(3);
-                }
+            p1finger3.setOnClickListener(view -> {
+                p1ArrayList.add(3);
             });
-//        p2click();
-            p2finger1.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    p2f1 = true;
-                    p2ArrayList.add(1);
-                }
+        //////////////// player 2 = red ////////////////
+            p2finger1.setOnClickListener(view -> {
+                p2ArrayList.add(1);
             });
-            p2finger2.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    p2f2 = true;
-                    p2ArrayList.add(2);
-                }
+            p2finger2.setOnClickListener(view -> {
+                p2ArrayList.add(2);
             });
-            p2finger3.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    p2f3 = true;
-                    p2ArrayList.add(3);
-                }
+            p2finger3.setOnClickListener(view -> {
+                p2ArrayList.add(3);
             });
 
-
-            cbtn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-//                    boolean arrayListCheck = p1ArrayList.equals(p2ArrayList);
-                    int roundInt;
-                    if (p1ArrayList.equals(p2ArrayList)) {
-                        roundInt = 1;
-                    } else {
-                        roundInt = -1;
-                    }
-                    round.setText(String.valueOf(roundInt));
+            cbtn.setOnClickListener(view -> {
+                int roundInt;
+                if (p1ArrayList.equals(p2ArrayList)) {
+                    roundInt = 1;
+                } else {
+                    roundInt = -1;
                 }
+                round.setText(String.valueOf(roundInt));
             });
-            rbtn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    gameInit(round);
-                }
-            });
-
+            rbtn.setOnClickListener(view -> gameInit(round, p1ArrayList, p2ArrayList));
 
     }
 
-    public void gameInit(TextView round){
-        p1f1 = false; p1f2 = false; p1f3 = false; p2f1 = false; p2f2 = false; p2f3 = false;
+    public void gameInit(TextView round, ArrayList<Integer> p2ArrayList, ArrayList<Integer> p1ArrayList){
         p1ArrayList.clear();
         p2ArrayList.clear();
         round.setText(String.valueOf(0));
     }
-//    public void p1click(){
-//        p1finger1.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                p1f1 = true;
-//            }
-//        });
-//        p1finger2.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                p1f2 = true;
-//            }
-//        });
-//        p1finger3.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                p1f3 = true;
-//            }
-//        });
-//    }
-//
-//    public void p2click(){
-//        p2finger1.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                p2f1 = true;
-//            }
-//        });
-//        p2finger2.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                p2f2 = true;
-//            }
-//        });
-//        p2finger3.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                p2f3 = true;
-//            }
-//        });
-//    }
 }
