41c5f3f9a84e29ea33aa6a53954bc627521d2b84
NoYoN1 <uilse1122@gmail.com>
2021-09-14 00:43:21 +0900

    sound

--- app/src/main/java/com/example/yubi/Game.java
+++ app/src/main/java/com/example/yubi/Game.java
@@ -2,29 +2,32 @@ package com.example.yubi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Build;
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
 import android.view.View;
-import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
 public class Game extends AppCompatActivity {
 
-    private Handler mHandler = new Handler();
-    private int mProgressStatus = 0;
-    int nextRound = 5;
+    private final Handler mHandler = new Handler();
+    int nextRound = 1;
+    boolean orderCheck = true, isOrderCheck = true;
+    int p1pointInt = 0, p2pointInt = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-
+        MediaPlayer correct = MediaPlayer.create(this, R.raw.correct);
+        MediaPlayer wrong = MediaPlayer.create(this, R.raw.wrong);
         /////////////// p1 = player1 ///////////////
         TextView p1finger1 = findViewById(R.id.p1finger1);
         TextView p1finger2 = findViewById(R.id.p1finger2);
@@ -71,35 +74,74 @@ public class Game extends AppCompatActivity {
             });
 //            nextRound++;
 //        }
-        progressBar.setMax(nextRound);
 
-        new Thread(() -> {
-            while(p1ArrayList.size() < 100){
-//                mProgressStatus++;
-                android.os.SystemClock.sleep(50);
-                mHandler.post(() -> progressBar.setProgress(p1ArrayList.size()));
-            }
-        }).start();
+            progressBar(progressBar, p1ArrayList, p2ArrayList, orderCheck);
 
-        arrayListCheck(checkButtonImg, p1ArrayList, p2ArrayList, round);
-        rbtnImg.setOnClickListener(view -> gameInit(round, p1ArrayList, p2ArrayList));
+            arrayListCheck(wrong, correct, checkButtonImg, p1ArrayList, p2ArrayList, orderCheck, p1point, p2point);
+
+            rbtnImg.setOnClickListener(view -> reset(p1ArrayList, p2ArrayList));
+
+
+
+//        orderCheck = setOrderCheck(isOrderCheck);
 
     }
-    public void arrayListCheck(ImageView checkButtonImg, ArrayList<Integer> p1ArrayList, ArrayList<Integer> p2ArrayList, TextView round){
+    ////////// if orderCheck == true => blue; if orderCheck == false => red; /////////////
+    private void progressBar( ProgressBar progressBar, ArrayList<Integer> p1ArrayList, ArrayList<Integer> p2ArrayList, boolean orderCheck) {
+        progressBar.setMax(nextRound);
+        if(orderCheck){
+            new Thread(() -> {
+                while(p1ArrayList.size() < 100){
+                    android.os.SystemClock.sleep(50);
+                    mHandler.post(() -> progressBar.setProgress(p1ArrayList.size()));
+                }
+            }).start();
+        }else{
+            new Thread(() -> {
+                while(p2ArrayList.size() < 100){
+                    android.os.SystemClock.sleep(50);
+                    mHandler.post(() -> progressBar.setProgress(p2ArrayList.size()));
+                }
+            }).start();
+        }
+    }
+
+    @SuppressLint("ResourceAsColor")
+    public void arrayListCheck(MediaPlayer wrong, MediaPlayer correct, ImageView checkButtonImg, ArrayList<Integer> p1ArrayList, ArrayList<Integer> p2ArrayList, boolean orderCheck, TextView p1point, TextView p2point){
         checkButtonImg.setOnClickListener(view -> {
-            int roundInt;
+            boolean correctMp3;
             if (p1ArrayList.equals(p2ArrayList)) {
-                roundInt = 2;
+                correctMp3 = true;
+                if(orderCheck){
+                    p1pointInt += 1;
+                    p1point.setText(String.valueOf(p1pointInt));
+                }else{
+                    p2pointInt += 1;
+                    p2point.setText(String.valueOf(p2pointInt));
+                }
             } else {
-                roundInt = -2;
+                correctMp3 = false;
             }
-            round.setText(String.valueOf(roundInt));
+            if(correctMp3){
+                correct.start();
+            }else{
+                wrong.start();
+            }
         });
     }
+    public void nextRound(){
 
-    public void gameInit(TextView round, ArrayList<Integer> p2ArrayList, ArrayList<Integer> p1ArrayList){
+    }
+    public boolean setOrderCheck(boolean isOrderCheck){
+        orderCheck = isOrderCheck;
+        return orderCheck;
+    }
+    public void point(){
+
+    }
+
+    public void reset(ArrayList<Integer> p2ArrayList, ArrayList<Integer> p1ArrayList){
         p1ArrayList.clear();
         p2ArrayList.clear();
-        round.setText(String.valueOf(1));
     }
 }
